0aa3a0d89dd133290af24016730f8dbb35db042a
Vitalii <patlitskiyvitaliy@gmail.com>
2024-01-25 23:31:00 +0200

    commit Auto

---
+++ git/c.bat
@@ -0,0 +1,5 @@
+git init
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/petlytskyiOne/db_light_Public.git
+git push -u origin main
---
+++ git/d.bat
@@ -0,0 +1,1 @@
+git clone https://github.com/petlytskyiOne/db_life.git
